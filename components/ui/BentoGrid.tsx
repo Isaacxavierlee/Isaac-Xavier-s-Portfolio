490f77ec0270e9c7f6604bc343d4353a19036888
Isaacxavierlee <reivax.caasi@gmail.com>
2024-05-28 21:42:42 +0800

    container

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -102,13 +102,26 @@ export const BentoGridItem = ({
 
       {/* Tech Stack */}
       {id === 3 && (
-        <div className="flex gap-1 lg:gap-5 w-full md:w-auto">
-          <div className="flex flex-wrap gap-3 md:gap-3 lg:gap-8">
+        <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+          <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
             {['React.js', 'Next.js', 'TypeScript'].map((item, index) => (
-              <span key={index} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-100 lg:opacity-100 rounded-lg text-center bg-[#10132E] text-white">
+              <span key={index} className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+              lg:opacity-100 rounded-lg text-center bg-[#10132E] text-white">
                 {item}
               </span>
             ))}
+
+            <span className='py-4 px-3 rounded-lg text-center bg-[#10132e]'/>
+            
+
+
+
+
+
+
+
+
+
           </div>
         </div>
       )}
