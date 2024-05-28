c3ef9b1c493761f2e7f0f7eb5c00f19d27d5f73a
Isaacxavierlee <reivax.caasi@gmail.com>
2024-05-28 23:21:15 +0800

    techstack

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -100,31 +100,38 @@ export const BentoGridItem = ({
 
       {id === 2 && <GlobeDemo />}
 
-      {/* Tech Stack */}
-      {id === 3 && (
-        <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-          <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
-            {['React.js', 'Next.js', 'TypeScript'].map((item, index) => (
-              <span key={index} className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-              lg:opacity-100 rounded-lg text-center bg-[#10132E] text-white">
-                {item}
-              </span>
-            ))}
+{/* Tech Stack */}
+{id === 3 && (
+  <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2" style={{ marginRight: "1rem" }}>
+    {/* Left Tech Stack List */}
+    <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+      {['React.js', 'Next.js', 'TypeScript'].map((item, index) => (
+        <span 
+          key={index} 
+          className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E] text-white"
+        >
+          {item}
+        </span>
+      ))}
+      <span className='py-4 px-3 rounded-lg text-center bg-[#10132E]' />
+    </div>
 
-            <span className='py-4 px-3 rounded-lg text-center bg-[#10132e]'/>
-            
+    {/* Right Tech Stack List */}
+    <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+      <span className='py-4 px-3 rounded-lg text-center bg-[#10132E]' />
+      {['MongoDB', 'Web3', 'JavaScript'].map((item, index) => (
+        <span 
+          key={index} 
+          className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E] text-white"
+        >
+          {item}
+        </span>
+      ))}
+    </div>
+  </div>
+)}
 
 
-
-
-
-
-
-
-
-          </div>
-        </div>
-      )}
     </div>
   );
 };
