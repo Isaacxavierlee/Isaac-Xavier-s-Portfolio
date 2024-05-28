fca872cd843841ffff7ccef01d11f631f64896b2
Isaacxavierlee <reivax.caasi@gmail.com>
2024-05-28 15:44:58 +0800

    griditems

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { gridItems } from '@/Data';
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid';
 
 const Grid = () => {
-  return (
-    <div>
-      Grid
-    </div>
-  )
-}
+   return (
+    <section id='about'>
+      <BentoGrid>
+        {gridItems.map((item) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
 
-export default Grid
+export default Grid;
