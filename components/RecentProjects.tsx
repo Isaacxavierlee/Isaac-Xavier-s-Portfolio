d73725fe778154d70fab1e8f77ff101e4e61af78
Isaacxavierlee <reivax.caasi@gmail.com>
2024-05-29 19:47:49 +0800

    recent prokjects update

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,26 +1,54 @@
 import { projects } from '@/Data';
 import React from 'react';
-import { PinContainer } from './ui/3d-pin'; // Ensure the correct path is used
+import { PinContainer } from './ui/3d-pin';
+import { FaLocationArrow } from 'react-icons/fa';
 
 const RecentProjects = () => {
   return (
-    <div className='py-20'>
-      <h1 className='heading text-white'>
-        A small selection of <span className='text-purple'>Recent Projects</span>
+    <div className="py-20">
+      <h1 className="heading text-white">
+        A small selection of <span className="text-purple">Recent Projects</span>
       </h1>
-      <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
-          <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw] text-white'>
-            <PinContainer title={title} href={link}>
-              <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh]'>
-                <div>
-                  <img src='/bg.png' alt='bg-img' />
+          <div
+            key={id}
+            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw] text-white"
+          >
+            <PinContainer title={link} href={link}>
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh]">
+                <img src={img} alt={title} className="z-10 absolute bottom-0" />
+              </div>
+
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1 transition duration-300 ease-in-out transform hover:text-purple hover:scale-105">
+                {title}
+              </h1>
+
+              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2 mt-2">
+                {des}
+              </p>
+
+              <div className="flex items-center justify-between mt-7 mb-3 w-full">
+                <div className="flex items-center space-x-2">
+                  {iconLists.map((icon, index) => (
+                    <div
+                      key={index}
+                      className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      style={{
+                        transform: `translateX(-${5 * index * 2}px)`,
+                      }}
+                    >
+                      <img src={icon} alt={icon} className="p-2" />
+                    </div>
+                  ))}
                 </div>
-                <img
-                src={img}
-                alt={title}
-                className='z-10 absolute bottom-0'
-                />
+
+                <div className="flex justify-center items-center">
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple hover:underline">
+                    View Live Site
+                  </p>
+                  <FaLocationArrow className="ml-2" color="#CBACF9" />
+                </div>
               </div>
             </PinContainer>
           </div>
@@ -28,6 +56,6 @@ const RecentProjects = () => {
       </div>
     </div>
   );
-}
+};
 
 export default RecentProjects;
