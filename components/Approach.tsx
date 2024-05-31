fdb2b5748fd7a04a2c0ca74b265fd2433739940f
Isaacxavierlee <reivax.caasi@gmail.com>
2024-05-31 16:50:51 +0800

    finished approaches

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -10,24 +10,24 @@ const Approach = () => {
       <h1 className="heading text-white text-center mb-10">
         My <span className="text-purple">Approach</span>
       </h1>
-      <div className="my-20 flex flex-col lg:flex-row items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-8">
+      <div className="my-20 flex flex-col lg:flex-row items-center justify-center w-full gap-4">
         <Card
-          title="Strategic Planning"
+          title="Planning & Strategy"
           icon={<AceternityIcon order="Phase 1" />}
           des="We'll collaborate to map out your website's goals, target audience, and key functionalities. We'll discuss things like site structure, navigation, and content requirements."
         >
           <CanvasRevealEffect
-            animationSpeed={10.2} // Increased speed
+            animationSpeed={5.1}
             containerClassName="bg-emerald-900 rounded-3xl overflow-hidden"
           />
         </Card>
         <Card
-          title="Development & Progress Updates"
+          title="Development & Progress Update"
           icon={<AceternityIcon order="Phase 2" />}
-          des="Once we agree on the plan, I cue my lofi playlist and dive into coding. From initial sketches to polished code, I keep you updated every step of the way."
+          des="Once we agree on the plan, I cue my playlist and dive into coding. From initial sketches to polished code, I keep you updated every step of the way."
         >
           <CanvasRevealEffect
-            animationSpeed={6} // Increased speed
+            animationSpeed={3}
             containerClassName="bg-pink-900 rounded-3xl overflow-hidden"
             colors={[
               [255, 166, 158],
@@ -37,12 +37,12 @@ const Approach = () => {
           />
         </Card>
         <Card
-          title="Implementation Following Approval"
+          title="Development & Launch"
           icon={<AceternityIcon order="Phase 3" />}
           des="This is where the magic happens! Based on the approved design, I'll translate everything into functional code, building your website from the ground up."
         >
           <CanvasRevealEffect
-            animationSpeed={6} // Increased speed
+            animationSpeed={3}
             containerClassName="bg-sky-600 rounded-3xl overflow-hidden"
             colors={[[125, 211, 252]]}
           />
@@ -58,7 +58,7 @@ const Card = ({ title, icon, children, des }) => {
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2] max-w-sm w-full mx-auto p-4 relative h-[35rem] rounded-3xl"
+      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2] max-w-sm w-full mx-auto p-4 relative lg:h-[35rem] rounded-3xl"
       style={{
         background: "rgb(4,7,29)",
         backgroundColor: "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
