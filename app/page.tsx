f79bc5fda0265723ed29cdae87264ace1771d5ce
Isaacxavierlee <reivax.caasi@gmail.com>
2024-05-31 10:55:19 +0800

    borders

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { FaHome } from "react-icons/fa";
 import RecentProjects from "@/components/RecentProjects"; // Added import for RecentProjects
 import { navItems } from "@/Data";
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience/>
       </div>
     </main>
   );
