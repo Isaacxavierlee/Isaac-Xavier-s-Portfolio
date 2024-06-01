1dda5fe84d7c8ceba9bb6b7c61905e3196d29e1e
Isaacxavierlee <reivax.caasi@gmail.com>
2024-06-01 23:20:31 +0800

    upgrading

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -99,12 +99,12 @@ const Card = ({ title, icon, children, des }) => {
 const AceternityIcon = ({ order }) => {
   return (
     <div>
-      <button className="relative inline-flex overflow-hidden rounded-full p-[1px]">
-        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-2 text-purple backdrop-blur-3xl font-bold text-2xl">
-          {order}
-        </span>
-      </button>
+      <button className="p-[3px] relative">
+      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+      <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+      {order}
+      </div>
+    </button>
     </div>
   );
 };
--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -1,13 +1,5 @@
 import React from "react";
 
-/**
- *  UI: border magic from tailwind css btns
- *  Link: https://ui.aceternity.com/components/tailwindcss-buttons
- *
- *  change border radius to rounded-lg
- *  add margin of md:mt-10
- *  remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
- */
 const MagicButton = ({
   title,
   icon,
@@ -23,20 +15,13 @@ const MagicButton = ({
 }) => {
   return (
     <button
-      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
+      className="p-[3px] relative"
       onClick={handleClick}
     >
-      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-
-      {/* remove px-3 py-1, add px-5 gap-2 */}
-      <span
-        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
-             bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
-      >
-        {position === "left" && icon}
+      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+      <div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
         {title}
-        {position === "right" && icon}
-      </span>
+      </div>
     </button>
   );
 };
